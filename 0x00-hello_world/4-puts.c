9ddab1115158317279afea33f9829bead46f10f1
Pintinz <osaivbieiguagbonmwen@gmail.com>
2023-02-16 06:30:21 -0800

    A C program that prints exactly programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - A program that prints a long string using puts function
+ * Return: 0 (success) !0 (not successful)
+ */
 int main(void)
 {
 	char name[]="\"Programming is like building a multilingual puzzle\n";
 	puts(%s, name);
 	return(0);
+}
