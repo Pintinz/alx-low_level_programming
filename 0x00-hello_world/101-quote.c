fb2c10462f4c3a49c8f2690506f25cdf3cfab1b1
Pintinz <osaivbieiguagbonmwen@gmail.com>
2023-02-16 08:03:43 -0800

    a C program that prints exactly and that piece of art is useful Dora Korpar 2015-10-19

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints standard error
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	char name;
+name = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fput("%s", name);
+return (1);
+}
