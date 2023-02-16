c1b0baa90e439c5941a0f2562f9afe0c48248b5a
Pintinz <osaivbieiguagbonmwen@gmail.com>
2023-02-16 08:09:06 -0800

    a C program that prints exactly and that piece of art is useful Dora Korpar 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,6 @@
  */
 int main(void)
 {
-	char name;
-name = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
