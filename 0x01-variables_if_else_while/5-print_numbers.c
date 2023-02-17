2db0dc49388f96508559a87cab1cf237729bff01
Pintinz <osaivbieiguagbonmwen@gmail.com>
2023-02-18 00:53:53 +0100

    5-numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int single_digit = 0;
+while (single_digit < 10)
+{
+printf("%d\n", single_digit);
+single_digit++;
+}
+return (0);
+}
