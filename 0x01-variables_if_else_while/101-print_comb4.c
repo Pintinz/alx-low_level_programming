b2bc9c249cc1943801765f2143c982caaaf52b8f
Pintinz <osaivbieiguagbonmwen@gmail.com>
2023-02-18 04:56:20 +0100

    11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,7 @@ for (k = j + 1; k < 10; k++)
 putchar(i + '0');
 putchar(j + '0');
 putchar(k + '0');
-if (!((i == 7) && (j == 8) && (k == 9)))
+if (i != 7 || j != 8 || k != 9)
 {
 putchar(',');
 putchar(' ');
