ab0d6c3dbd8aa6058b3ada5310c4a5750ee7c150
Pintinz <osaivbieiguagbonmwen@gmail.com>
2023-02-21 06:52:49 -0800

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - prints lowercase letters
+ * @c - is a interger of lower case
+ * Return: 1 if c is a lowercase and 0 if otherwise
+ */
+int _islower(int c) /*c is an interger that represent lower case*/
+{
+	if (c == 'c')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
