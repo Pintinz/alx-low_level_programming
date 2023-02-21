c7c548283c7aaf16d97e4cb0ff9f1c91ea72b370
Pintinz <osaivbieiguagbonmwen@gmail.com>
2023-02-21 08:48:12 -0800

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,18 +8,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		return (0);
 		_putchar('0');
+		return (0);
 	}
 	else if (n < 0)
 	{
-		return (-1);
-		_putchar('-');
+		 _putchar('-');
+		return (1);
 	}
 	return (0);
 }
