b276fcab799ec7fc88a9785a3844176e0894694e
Pintinz <osaivbieiguagbonmwen@gmail.com>
2023-02-21 07:52:24 -0800

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _isalpha - function that calls out an int c
+ * @c: c represent a letter, lowercase or uppercase
+ *
+ * Return: always 0.
+ */
+int _isalpha(int c)
+{
+	if (c == 'H')
+	{
+		return (1);
+	}
+	else if (c == 'o')
+	{
+		return (1);
+	}
+	else if (c == 'l')
+	{
+		return (1);
+	}
+	else if (c == 's')
+	{
+		return (1);
+	}
+	else if (c == 'n')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
