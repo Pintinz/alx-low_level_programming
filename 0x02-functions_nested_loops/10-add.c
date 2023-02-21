d137bb536edf970f4de7beea4ee192acc1b94752
Pintinz <osaivbieiguagbonmwen@gmail.com>
2023-02-21 11:15:50 -0800

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - function that calls int i and l.
+ * @i: the second interger
+ * @l: the second interger.
+ * Return: always 0
+ */
+int add(int i, int l)
+{
+	int n;
+	n = i + l;
+	return (n);
+}
