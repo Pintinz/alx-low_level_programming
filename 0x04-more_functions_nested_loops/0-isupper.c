05525cc37dfe28d1d1dfe301bc625d6ecddf3ab9
Pintinz <osaivbieiguagbonmwen@gmail.com>
2023-02-23 10:15:49 -0800

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,18 +8,15 @@
 
 int _isupper(int c)
 {
-	char i;
 
-	for (i = 'A'; i <= 'Z'; i++)
+	if (c <= 'Z' && c >= 'A')
 	{
-		if (c == i)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		return (1);
 	}
+	else
+	{
+		return (0);
+	}
+
 	return (0);
 }
