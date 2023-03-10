ada5a735867a0272d5477a74578a0dc0ea462332
Pintinz <osaivbieiguagbonmwen@gmail.com>
2023-03-10 17:12:09 +0100

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,11 +2,11 @@
 /**
  * main - check the code
  *
- * Return: 0
+ * Return: Always 0
  */
 
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	return (0);
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
 }
