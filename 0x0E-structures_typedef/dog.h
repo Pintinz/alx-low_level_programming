a6088ba9bbb5f6758378da1d8b5109e6d44f70f8
Pintinz <osaivbieiguagbonmwen@gmail.com>
2023-03-20 20:48:37 +0100

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #ifndef HEADER_FILE
-#define HEADER_FILE  dog.h
+#define HEADER_FILE dog.h
 
 /**
  * struct dog - Define a new struct dog with name, age, owner
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of dog
- **/
+ */
 
 struct dog
 {
