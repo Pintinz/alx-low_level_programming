7056db90cc0af6128cb23b05119ff9f5c919844e
Pintinz <osaivbieiguagbonmwen@gmail.com>
2023-03-20 20:39:56 +0100

    0. Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is the README file of 0x0E-structures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#ifndef DOG_H
+#define DOG_H
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
