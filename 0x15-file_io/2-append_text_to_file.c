118ea7e37b7272ac17aa0ebed7105e878c337eb3
Your Name <you@example.com>
2023-04-09 14:02:36 -0700

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	else if (text_content != NULL)
 	{
-		while (text_content[len])
+		while (text_content[length])
 		{
 			length++;
 		}
